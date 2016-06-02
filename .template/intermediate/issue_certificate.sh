a10875d130893cf1a28f731038f7510f210de096
Josh Jones <jjones@pltnm.com>
2016-06-02 16:45:57 -0500

    Updated issue_certificate script to issue both server and client certs.

--- .template/intermediate/issue_certificate.sh
+++ .template/intermediate/issue_certificate.sh
@@ -2,15 +2,17 @@
 
 # Helper Functions
 printUsage() {
-  echo "Usage: issue_certificate.sh <directory to place certificate> [path to csr file]"
+  echo "Usage: issue_certificate.sh <directory to place certificate> <type of certificate (options: server, client)> [path to csr file]"
   echo ""
   return 0
 }
 
 # Parameters
 outputDir=$1
-csrFile=$2
+certType=$2
+csrFile=$3
 caDirectory=`dirname $0`
+dateStamp=`date +%Y-%m-%dT%H-%M-%S`
 
 if [[ ($1 == "help") || ($1 == "-help") || ($1 == "-h") || ($1 == "--help") ]]; then
   printUsage
@@ -24,8 +26,22 @@ if [[ -z $outputDir ]]; then
   exit 1
 fi
 
+case $certType in
+  server)
+    ca_extension="server_cert"
+    ;;
+  client)
+    ca_extension="client_cert"
+    ;;
+  *)
+    printUsage
+
+    echo "ERROR: Certificate type is not one of the available options."
+    exit 1
+esac
+
 # Print Parameters
-echo "Creating certificate in $outputDir using certificate authority at $caDirectory."
+echo "Creating $certType certificate in $outputDir using certificate authority at $caDirectory."
 
 # Ensure the output directory exists
 mkdir -p $outputDir
@@ -34,19 +50,23 @@ if [[ -z $csrFile ]]; then
   echo "No CSR provided: generating a private key for this certificate."
 
   # Generate a CSR since one was not provided to us.
-  openssl genrsa -out "$outputDir/private.key" 2048
-  openssl req -new -key "$outputDir/private.key" -out "$outputDir/request.csr"
-  csrFile="$outputDir/request.csr"
+  openssl genrsa -out "$outputDir/private.key.pem" 2048
+  openssl req -config "$caDirectory/openssl.conf" -new -sha256 -key "$outputDir/private.key.pem" -out "$outputDir/request.csr.pem"
+  csrFile="$outputDir/request.csr.pem"
   csrGenerated=true
 fi
 
+# Make a copy of the CSR for future reference
+csrFileName=`basename $csrFile`
+cp $csrFile "$caDirectory/csr/${csrFileName%%.*}-$dateStamp.csr.pem"
+
 # Generate the certificate
-openssl x509 -req -in $csrFile -CA "$caDirectory/rootCA.pem" -CAkey "$caDirectory/rootCA.key" -CAcreateserial -out "$outputDir/public.pem" -days 365 -sha256
+openssl ca -config "$caDirectory/openssl.conf" -extensions $ca_extension -days 375 -notext -md sha256 -in $csrFile -out "$outputDir/public.cert.pem"
 
 # Generate a PKCS#12 archive if we generated the CSR
 if [[ $csrGenerated ]]; then
   echo "Exporting pkcs#12 archive of generated certificate and key..."
   echo "Entering a password may be required by some utilities when importing (ie. OS X Keychain)."
 
-  openssl pkcs12 -export -out "$outputDir/archive.p12" -inkey "$outputDir/private.key" -in "$outputDir/public.pem" -certfile "$caDirectory/rootCA.pem"
+  openssl pkcs12 -export -out "$outputDir/archive.p12" -inkey "$outputDir/private.key.pem" -in "$outputDir/public.cert.pem" -certfile "$caDirectory/certs/ca-chain.cert.pem"
 fi
